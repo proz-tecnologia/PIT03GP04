3de9b2d80c52575a131db263ba3c2ef4a89b55e6
Ericson <ericsonsergio19844@gmail.com>
2022-10-26 21:48:25 -0300

    grafico_pizza
    Foi desenvolvido o gráfico pizza.

---
+++ lib/src/ui/pages/home/widgets/grafico_pizza.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class GraficoPizza extends StatefulWidget {
+  const GraficoPizza({super.key});
+
+  @override
+  State<GraficoPizza> createState() => _GraficoPizzaState();
+}
+
+class _GraficoPizzaState extends State<GraficoPizza> {
+  int index = 0;
+
+  Map<String, double> listaValores = {
+    'Alimentação': 667.89,
+    'Transporte': 550.76,
+    'Diversão': 238.99,
+    'Educação': 235.99,
+    'Saúde': 980.55
+  };
+
+  List<Color> listaCores = [
+    const Color(0xffD95AF3),
+    const Color(0xff3EE094),
+    const Color(0xff3398F6),
+    const Color(0xffFA4A42),
+    const Color(0xffFE9539)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Gráfico Pizza'),
+      ),
+      body: Center(
+        child: PieChart(
+          dataMap: listaValores,
+          colorList: listaCores,
+          //Definindo o tamanho da pizza
+          chartRadius: MediaQuery.of(context).size.width / 2,
+          centerText: 'Pd escrever algo no meio',
+          chartType: ChartType.ring,
+          ringStrokeWidth: 24,
+          animationDuration: const Duration(seconds: 3),
+          chartValuesOptions: const ChartValuesOptions(
+              //mostrar valores
+              showChartValues: true,
+              //mostrar valores fora do grafico
+              showChartValuesOutside: true,
+              //mostrar valores em porcentagem
+              showChartValuesInPercentage: false,
+              //mostrar o fundo dos valores
+              showChartValueBackground: false),
+          legendOptions: const LegendOptions(
+              showLegends: true,
+              legendShape: BoxShape.circle,
+              legendTextStyle: TextStyle(fontSize: 16)),
+        ),
+      ),
+    );
+  }
+}
