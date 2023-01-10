3276ba5db6cd3d492a673112b9a844319f6352a5
Wellington Brito <wellington.brito@rocketmail.com>
2023-01-09 22:06:06 -0300

    Inserindo contas a receber

--- lib/contas_pagar/ui/pages/contas_pagar.dart
+++ lib/contas_pagar/ui/pages/contas_pagar.dart
@@ -15,74 +15,76 @@ class ContasPagar extends StatelessWidget {
       drawer: const DrawerView(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Center(
-              child: Image.asset('assets/despesas.png'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Despesas",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.red,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Image.asset('assets/despesas.png'),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                debugPrint('Teste');
-              },
-              backgroundColor: Colors.red,
-              child: const Icon(Icons.add),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Divider(
-              height: 40,
-              thickness: 2,
-              indent: 10,
-              endIndent: 10,
-              color: Colors.red,
-            ),
-            Container(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: const [
-                    SizedBox(
-                      height: 20,
-                    ),
-                    CardValuesReleases(),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CardValuesReleases(),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CardValuesReleases(),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CardValuesReleases(),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CardValuesReleases(),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    CardValuesReleases(),
-                  ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Despesas",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.red,
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  debugPrint('Teste');
+                },
+                backgroundColor: Colors.red,
+                child: const Icon(Icons.add),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Divider(
+                height: 40,
+                thickness: 2,
+                indent: 10,
+                endIndent: 10,
+                color: Colors.red,
+              ),
+              Container(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: const [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/contas_receber/ui/pages/contas_receber.dart
+++ lib/contas_receber/ui/pages/contas_receber.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../../../login/ui/component/drawer_page.dart';
+import 'package:four_finance_app/home/ui/component/cards_values_releases.dart';
+import 'package:four_finance_app/login/ui/component/drawer_page.dart';
 
 class ContasReceber extends StatelessWidget {
   const ContasReceber({super.key});
@@ -10,15 +10,82 @@ class ContasReceber extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: const Text('Home Page'),
-        centerTitle: true,
+        title: const Text('Contas a Receber'),
       ),
       drawer: const DrawerView(),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 10.0),
-        // Decoration -> BoxDecoration
-        // https://api.flutter.dev/flutter/painting/BoxDecoration-class.html
-        child: Text("Página de contas a receber"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Image.asset('assets/receitas.png'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Receitas",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.red,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  debugPrint('Teste');
+                },
+                backgroundColor: Colors.green,
+                child: const Icon(Icons.add),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Divider(
+                height: 40,
+                thickness: 2,
+                indent: 10,
+                endIndent: 10,
+                color: Colors.red,
+              ),
+              Container(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: const [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CardValuesReleases(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
